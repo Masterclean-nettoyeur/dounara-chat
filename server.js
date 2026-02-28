868e60b9dc90e3c5a93809319ce672a7acbb6fce
Masterclean-nettoyeur <doukingmbaye232@gmail.com>
2026-02-28 18:26:32 +0000

    Enhance server with image handling and album feature
    Added functionality to handle image uploads and maintain an album.

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 let users = [];
+let images = []; // stocke les images pour l'album
 
 io.on("connection", (socket) => {
   socket.on("join", (name) => {
     if (users.length < 2) {
       users.push({ id: socket.id, name });
       socket.emit("joined", name);
+      socket.emit("album", images); // envoyer l'album existant
     } else {
       socket.emit("full");
     }
@@ -21,10 +23,17 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("message", msg);
   });
 
+  socket.on("image", (imgData) => {
+    images.push(imgData); // sauvegarde dans l'album
+    socket.broadcast.emit("image", imgData);
+    io.emit("album", images); // mise à jour album pour tous
+  });
+
   socket.on("disconnect", () => {
     users = users.filter(u => u.id !== socket.id);
   });
 });
+
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log("DouNara lancé sur le port " + PORT);
